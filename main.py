421eb69e8285746c2d7261126ffa1547235631cf
mhshihnuu <mhshih@nuu.edu.tw>
2025-04-27 16:48:38 +0800

    Skipped undefined group function2457.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from p1 import f1
-from p2 import f2
-from p3 import f3
+from G1 import f1
+#from G2 import f2
+from G3 import f3
+#from G4 import f4
+#from G5 import f5
+from G6 import f6
+#from G7 import f7
+from G8 import f8
 
-n1 = f1()
-n2 = f2(n1)
-n3 = f3(n2)
-print(n3)
+n1 = f1(0)
+#n2 = f2(n1)
+n3 = f3(n1)
+#n4 = f4(n3)
+#n5 = f4(n4)
+n6 = f6(n3)
+#n7 = f7(n6)
+n8 = f8(n6)
+
+print(n8)
